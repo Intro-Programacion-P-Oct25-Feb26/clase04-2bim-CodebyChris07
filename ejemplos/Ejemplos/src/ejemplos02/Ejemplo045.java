34941bc5a4f2fe12c5dc55d461299a45f46d3567
CodebyChris07 <chrisjalu07@gmail.com>
2026-01-08 10:32:20 -0500

    arreglo ejemplos2

--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo045.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo045.java
@@ -16,27 +16,40 @@ public class Ejemplo045 {
 
         int[][] arreglo1 = {{1, 2, 3}, {3, 2, 4}, {2, 6, 2}};
         int[][] arreglo2 = {{1, 2, 3}, {2, 2, 2}, {3, 1, 2}};
-        int[][] arreglo3 = llenarMatriz(arreglo1, arreglo2);
-        double[][] arreglo4 = llenarMatriz2(arreglo1, arreglo2);
-        // int [][] arreglo3 = new int[3][3];
+        int[][] arreglo3 = matriz3(arreglo1, arreglo2);
+        double[][] arreglo4 = matriz4(arreglo1, arreglo2);
 
         obtenerReporte(arreglo1);
         obtenerReporte(arreglo2);
         obtenerReporte(arreglo3);
-        obtenerReporte(arreglo4);
+        obtenerReporteDos(arreglo4);
     }
 
-    public static int[][] llenarMatriz(int[][] x, int[][] y) {
-        int[][] z = new int[3][3];
-        for (int i = 0; i < x.length; i++) {
-            for (int j = 0; j < x[i].length; j++) {
-                z[i][j] = obtenerMultiplicacion(x[i][j],
-                        y[i][j]); 
+    public static int[][] matriz3(int[][] a, int[][] b) {
+        int[][] matriz = new int[3][3];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+
+                matriz[i][j] = obtenerMultiplicacion(a[i][j],
+                        b[i][j]);
             }
         }
-        return z;
+
+        return matriz;
     }
-    
+
+    public static double[][] matriz4(int[][] a, int[][] b) {
+        double[][] matriz = new double[3][3];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+
+                matriz[i][j] = obtenerPot(a[i][j],
+                        b[i][j]);
+            }
+        }
+
+        return matriz;
+    }
 
     public static void obtenerReporte(int[][] arreglo1) {
         String cadena = "";
@@ -48,21 +61,27 @@ public class Ejemplo045 {
         }
         System.out.println(cadena);
     }
-
+    public static void obtenerReporteDos(double[][] arreglo1) {
+        String cadena = "";
+        for (int i = 0; i < arreglo1.length; i++) {
+            for (int j = 0; j < arreglo1[i].length; j++) {
+                cadena = String.format("%s%s\t", cadena, arreglo1[i][j]);
+            }
+            cadena = String.format("%s\n", cadena);
+        }
+        System.out.println(cadena);
+    }
     public static int obtenerMultiplicacion(int a, int b) {
         int operacion;
         operacion = a * b;
         return operacion;
 
     }
-    public static double obtenetPotencia (int a, int b){
-    double potencia;
-    a = (int)(a * 1.00);
-    b = (int)(b * 1.00);
-    potencia = Math.pow(a, b); 
-    
-    
-    
+
+    public static double obtenerPot(int a, int b) {
+        double operacion; 
+        operacion = Math.pow(a, b);
+        return operacion;
     }
 
 }
